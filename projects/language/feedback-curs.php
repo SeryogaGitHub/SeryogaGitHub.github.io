bfc839ba0cc4aff68a0db5b7cb1c32cc91cc0456
SeryogaGitHub <main.email.seryoga@gmail.com>
2018-11-25 21:51:09 +0200

    new change

---
+++ projects/language/feedback-curs.php
@@ -0,0 +1,48 @@
+﻿<?php
+
+if (!empty($_POST['phone']) ) {
+    $return_message = "";
+    $to = "main.email.seryoga@gmail.com";
+    $subject = 'Заказ курса';
+
+    $phone = $_POST['phone'];
+    $phone = trim($phone);
+    $phone = htmlspecialchars($phone);
+
+    $curs = $_POST['curs'];
+    $curs = trim($curs);
+    $curs = htmlspecialchars($curs);
+
+    $name = $_POST['name'];
+    $name = trim($name);
+    $name = htmlspecialchars($name);
+
+    $message = "
+    <html> 
+        <head> 
+            <title>$subject</title> 
+        </head> 
+        <body>
+          <table>
+            <tr><td><b>Курс: </b></td><td>$curs</td></tr>
+            <tr><td><b>Имя: </b></td><td>$name</td></tr>
+            <tr><td><b>Телефон: </b></td><td>$phone</td></tr>
+          </table>
+        </body> 
+    </html>"; 
+
+    $headers = "From: noreply@courses.arabic-school.org/\r\n";
+    $headers .= "Content-type: text/html; charset=utf-8 \r\n";
+    $send_mail = mail($to, $subject, $message, $headers); 
+
+    if ( $send_mail ) {
+      $return_message = "mail success";
+    }
+    else {
+      $return_message = "mail error";
+    }
+    echo $return_message;
+  }
+
+exit();
+?>
---
+++ projects/language/feedback.php
@@ -0,0 +1,43 @@
+﻿<?php
+
+if (!empty($_POST['phone']) ) {
+    $return_message = "";
+    $to = "main.email.seryoga@gmail.com";
+    $subject = 'Номер который заказал обратный звонок';
+
+    $phone = $_POST['phone'];
+    $phone = trim($phone);
+    $phone = htmlspecialchars($phone);
+
+    $name = $_POST['name'];
+    $name = trim($name);
+    $name = htmlspecialchars($name);
+
+    $message = "
+    <html> 
+        <head> 
+            <title>$subject</title> 
+        </head> 
+        <body>
+          <table>
+            <tr><td><b>Имя: </b></td><td>$name</td></tr>
+            <tr><td><b>Телефон: </b></td><td>$phone</td></tr>
+          </table>
+        </body> 
+    </html>"; 
+
+    $headers = "From: noreply@courses.arabic-school.org/\r\n";
+    $headers .= "Content-type: text/html; charset=utf-8 \r\n";
+    $send_mail = mail($to, $subject, $message, $headers); 
+
+    if ( $send_mail ) {
+      $return_message = "mail success";
+    }
+    else {
+      $return_message = "mail error";
+    }
+    echo $return_message;
+  }
+
+exit();
+?>
