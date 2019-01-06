37d23145e05fd10517b9d18e7e6fd49c97f3f018
SeryogaGitHub <main.email.seryoga@gmail.com>
2019-01-06 16:52:41 +0200

    new

--- resume/js/scripts.js
+++ resume/js/scripts.js
@@ -2,39 +2,62 @@
 
 
 $(document).ready(function() {
-    $("#main-header").load("header.html");
-    $("#main-footer").load("footer.html");
+  // півдключити футер і хедер
+  var mainHeader = $("#main-header");
 
-    // анімації
-    new WOW().init();
+  mainHeader.load("header.html");
+  $("#main-footer").load("footer.html");
 
-    // перевірка чи тачпад прилад
-    var touchSupport = 'ontouchstart' in window || window.DocumentTouch && document instanceof window.DocumentTouch;
-    
-    if (touchSupport){
-        $('.item').each(function(){
-            var pages = $(this).find('.pages').length;
+  // активна сторінка 
+  var linkPage = mainHeader.data('page');
 
-            if(pages){
+  $(window).load(function (){
+    $('.navigation a').each(function(){
+      var href = $(this).attr('href');
 
-                $(this).children('a').addClass('disabled');
-                $(this).addClass('click');
-            }
-        });
+      if(href.indexOf(linkPage) > 0){
+        $(this).addClass('active');
+        return;
+      }
+    });
+  });
+
+  // анімації
+  // new WOW().init();
+
+  var wow = new WOW({
+    mobile: false,
+  })
+
+  wow.init();
+
+  // перевірка чи тачпад прилад
+  var touchSupport = 'ontouchstart' in window || window.DocumentTouch && document instanceof window.DocumentTouch;
+  
+  if (touchSupport){
+    $('.item').each(function(){
+      var pages = $(this).find('.pages').length;
+
+      if(pages){
+
+        $(this).children('a').addClass('disabled');
+        $(this).addClass('click');
+      }
+    });
+  }
+
+  $('.grid').on('click', '.item.click', function(){
+    var pages = $(this).find('.pages').length;
+
+    $('.item.click').not(this).removeClass('open');
+    if(pages){
+
+      $(this).toggleClass('open');
     }
+  });
 
-    $('.grid').on('click', '.item.click', function(){
-        var pages = $(this).find('.pages').length;
-
-        $('.item.click').not(this).removeClass('open');
-        if(pages){
-
-            $(this).toggleClass('open');
-        }
-    });
-
-    // відключаємо стандартні дії зсилки яка має .disabled
-    $('body').on('click', 'a.disabled', function(e){
-        e.preventDefault();
-    });
+  // відключаємо стандартні дії зсилки яка має .disabled
+  $('body').on('click', 'a.disabled', function(e){
+    e.preventDefault();
+  });
 });
