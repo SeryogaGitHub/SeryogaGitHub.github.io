d63a300e2010fdecc9f4eb9a51588e36b3ca2740
Seryoga <main.email.seryoga@gmail.com>
2019-03-12 18:38:51 +0200

    test

---
+++ griph/js/scripts.js
@@ -0,0 +1,19 @@
+'use strict';
+
+$(document).ready(function() {
+  $('.popup').magnificPopup({
+    type: 'inline',
+
+    fixedContentPos: false,
+    fixedBgPos: true,
+
+    overflowY: 'auto',
+
+    closeBtnInside: true,
+    preloader: false,
+    
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'mfp-zoom-in'
+  });
+});
