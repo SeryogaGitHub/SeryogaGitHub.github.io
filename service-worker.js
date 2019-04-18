34eebb10c769f60bfc0687b1812636f90726e928
Seryoga <main.email.seryoga@gmail.com>
2019-04-18 18:16:00 +0300

    new

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ae445e9457c29386650643ad4d08844f.js"
+  "/precache-manifest.c7018c6fb494d576daa2f796fd6dd4c5.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "resume"});
