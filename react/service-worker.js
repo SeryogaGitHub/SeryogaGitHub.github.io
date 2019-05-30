5328cba2a6133022204db05181b1f225b5c63847
Seryoga <main.email.seryoga@gmail.com>
2019-05-30 17:48:59 +0300

    react

--- react/service-worker.js
+++ react/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "./resume/precache-manifest.ae445e9457c29386650643ad4d08844f.js"
+  "/precache-manifest.c34d3c762d9fb28303f6fc6d2346cca2.js"
 );
 
-workbox.core.setCacheNameDetails({prefix: "resume"});
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,5 +31,9 @@ workbox.core.setCacheNameDetails({prefix: "resume"});
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
