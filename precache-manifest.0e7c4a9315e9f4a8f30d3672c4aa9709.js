f504f5711d1ad45531edf6581842ade17ce4e111
SeryogaGitHub <main.email.seryoga@gmail.com>
2019-12-25 12:24:29 +0200

    React resume

---
+++ precache-manifest.0e7c4a9315e9f4a8f30d3672c4aa9709.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f53ca420b7f981d769af9372c78f5773",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53400b9d10087cbe3f1e",
+    "url": "/static/css/main.459cac5a.chunk.css"
+  },
+  {
+    "revision": "a15481b8a5d716c7e693",
+    "url": "/static/js/2.6ab9d854.chunk.js"
+  },
+  {
+    "revision": "53400b9d10087cbe3f1e",
+    "url": "/static/js/main.f88906d4.chunk.js"
+  },
+  {
+    "revision": "55ded5f4b7817a2fd4fa",
+    "url": "/static/js/runtime-main.934c9442.js"
+  },
+  {
+    "revision": "306883355153806e98cb262988227701",
+    "url": "/static/media/avatar.30688335.jpg"
+  },
+  {
+    "revision": "1e5211235cdea6a97bc72fd5a7fca83e",
+    "url": "/static/media/lato-regular.1e521123.eot"
+  },
+  {
+    "revision": "81f40287a6b4768cdd38c17401e5c8c9",
+    "url": "/static/media/lato-regular.81f40287.svg"
+  },
+  {
+    "revision": "c0d37a3b8ba0f48d462508aa6c589643",
+    "url": "/static/media/lato-regular.c0d37a3b.woff"
+  },
+  {
+    "revision": "ca5e32a8a9daa9af473f2cf082d6702e",
+    "url": "/static/media/lato-regular.ca5e32a8.ttf"
+  },
+  {
+    "revision": "d7b16a7a9bd8f8752c7603210c7a8d32",
+    "url": "/static/media/lato-regular.d7b16a7a.woff2"
+  },
+  {
+    "revision": "b944492960d285d9c71424704eecbacd",
+    "url": "/static/media/main-bg-mb.b9444929.jpg"
+  },
+  {
+    "revision": "7db1699a875f2cb59a60cfa069837e07",
+    "url": "/static/media/main-bg.7db1699a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7d4be22262fc7b5150827fb2186d7651.js"
+  "/precache-manifest.0e7c4a9315e9f4a8f30d3672c4aa9709.js"
 );
 
-workbox.core.setCacheNameDetails({prefix: "resume"});
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,5 +31,9 @@ workbox.core.setCacheNameDetails({prefix: "resume"});
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
